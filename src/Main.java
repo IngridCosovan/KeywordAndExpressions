a1ade853792d2444fb37096992dce933b06cf187
Ingrid Cosovan <ingrid.cosovan@gmail.com>
2021-11-10 22:32:10 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        //expresie ilometers = (100 * 1.609344)
+        // kilometers= variabila
+        // valoare 101.609344
+        // operators =, *
+        //Expresion componentd are: variables, values and operators
+
+        int highScore = 50; //expression: highScore = 50
+        if (highScore == 50) { //expression :highScore == 50
+            System.out.println("This is an expression"); //expresion: his is an expression
+
+
+
+            //Exercitiu: In the following code htat will type below, write down what parts of the code are expressions
+
+            int score = 100; //expresions: score = 100
+            if (score >99) { //expresion: score > 99
+                System.out.println("You got the high score"); //expression: You got the high score
+                score = 0; //expression: score = 0;
+
+
+                //keywords: https://en.wikipedia.org/wiki/List_of_Java_keywords
+            }
+        }
+    }
